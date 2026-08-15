a6de497f23b653491d78e896d5456af30dede677
Karthikeyan C <karthikeyancc@yahoo.com>
2026-08-16 00:00:54 +0530

    cumulative sum return type change

--- src/app/utils/data-util.ts
+++ src/app/utils/data-util.ts
@@ -359,7 +359,7 @@ export class ChartUtils {
     }
   }
 
-  aggregate(cfg: Record<string, any>, val: number | number[]| null, items: number[]) {
+  aggregate(cfg: Record<string, any>, val: number | null, items: number[]) {
     switch (cfg['agg']) {
       case 'Number':
         val = items.length ? items[0] : null;
@@ -374,7 +374,10 @@ export class ChartUtils {
         val = items.reduce((a, b) => a + b, 0);
         break;
       case 'Cumulative Sum':
-        let cumulative = 0; val = items.map(item => { cumulative += item; return cumulative;});
+        val = items.map((item, i) =>
+            items.slice(0, i + 1).reduce((sum, value) => sum + value, 0)
+        )[0];
+        //optimal way let cumulative = 0; val = items.map(item => { cumulative += item; return cumulative;});
         break;
       case 'Average':
         val = items.length ? items.reduce((a, b) => a + b, 0) / items.length : 0;
